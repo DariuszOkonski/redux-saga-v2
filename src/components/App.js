fc1af6c0d1d94cd8cfb9ed925d01cf0c22c6d6d6
Dariusz Okoński <darek200180@gmail.com>
2024-09-19 20:36:19 +0200

    add NewUserForm

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsersRequest } from '../actions/users';
 import UsersList from './UsersList';
+import NewUserForm from './NewUserForm';
 
 class App extends Component {
   constructor(props) {
@@ -13,10 +14,11 @@ class App extends Component {
   render() {
     const users = this.props.users;
 
-    console.log(users);
+    // console.log(users);
 
     return (
       <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <NewUserForm />
         <UsersList users={users.items} />
       </div>
     );
