7fcbd3939a0ab77e7ee5fa29346eefedb4a36f30
Dariusz Okoński <darek200180@gmail.com>
2024-09-19 20:22:06 +0200

    display users

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>Hello</h1>;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getUsersRequest } from '../actions/users';
+import UsersList from './UsersList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.getUsersRequest();
+  }
+
+  render() {
+    const users = this.props.users;
+
+    console.log(users);
+
+    return (
+      <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <UsersList users={users.items} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(({ users }) => ({ users }), {
+  getUsersRequest,
+})(App);
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 axios.defaults.baseURL = 'http://localhost:3004';
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
