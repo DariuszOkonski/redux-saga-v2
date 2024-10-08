c87fc72abd2afa1ffed2d420ee809b3955d93aa3
Dariusz Okoński <darek200180@gmail.com>
2024-10-08 21:01:50 +0200

    hook up App with saga

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getUsersRequest } from '../actions/users';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.getUsersRequest();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +18,6 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, {
+  getUsersRequest,
+})(App);
