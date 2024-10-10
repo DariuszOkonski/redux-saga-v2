3c6ec6f23ce8c9bc56c2567ea8c53925cc5eeaa1
Dariusz Okoński <darek200180@gmail.com>
2024-10-10 19:32:14 +0200

    clean up code

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {
-  getUsersRequest,
-  createUserRequest,
-  deleteUserRequest,
-  usersError,
-} from '../actions/users';
-import UsersList from './UsersList';
-import NewUserForm from './NewUserForm';
-import { Alert } from 'reactstrap';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.props.getUsersRequest();
-  }
-
-  handleSubmit = ({ firstName, lastName }) => {
-    this.props.createUserRequest({ firstName, lastName });
-  };
-
-  handleDeleteUserClick = (userId) => {
-    this.props.deleteUserRequest(userId);
-  };
-
-  handleCloseAlert = () => {
-    this.props.usersError({ error: '' });
-  };
-
   render() {
-    const users = this.props.users;
-
     return (
       <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
-        <Alert
-          color='danger'
-          isOpen={!!this.props.users.error}
-          toggle={this.handleCloseAlert}
-        >
-          {this.props.users.error}
-        </Alert>
-        <NewUserForm onSubmit={this.handleSubmit} />
-        <UsersList
-          onDeleteUser={this.handleDeleteUserClick}
-          users={users.items}
-        />
+        <h1>Hello World</h1>
       </div>
     );
   }
 }
 
-export default connect(({ users }) => ({ users }), {
-  getUsersRequest,
-  createUserRequest,
-  deleteUserRequest,
-  usersError,
-})(App);
+export default App;
