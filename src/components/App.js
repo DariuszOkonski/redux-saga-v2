00645efedc904ff9233091fd4e4f17d32d74bc35
Dariusz Okoński <darek200180@gmail.com>
2024-10-10 19:25:41 +0200

    add errors handling

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,11 @@ import {
   getUsersRequest,
   createUserRequest,
   deleteUserRequest,
+  usersError,
 } from '../actions/users';
 import UsersList from './UsersList';
 import NewUserForm from './NewUserForm';
+import { Alert } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
@@ -23,11 +25,22 @@ class App extends Component {
     this.props.deleteUserRequest(userId);
   };
 
+  handleCloseAlert = () => {
+    this.props.usersError({ error: '' });
+  };
+
   render() {
     const users = this.props.users;
 
     return (
       <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <Alert
+          color='danger'
+          isOpen={!!this.props.users.error}
+          toggle={this.handleCloseAlert}
+        >
+          {this.props.users.error}
+        </Alert>
         <NewUserForm onSubmit={this.handleSubmit} />
         <UsersList
           onDeleteUser={this.handleDeleteUserClick}
@@ -42,4 +55,5 @@ export default connect(({ users }) => ({ users }), {
   getUsersRequest,
   createUserRequest,
   deleteUserRequest,
+  usersError,
 })(App);
